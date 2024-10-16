732644aa6a3b50ca35d1baa1d0afc2bf9b5df79c
gribbib <j.git@motschi.email>
2024-10-16 09:56:34 +0200

    quiz-02

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,17 +27,52 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String,Command)>) -> Vec<String> { 
+        let mut return_vector: Vec<String> = Vec::new();
+        for tuple in input {
+            match tuple.1 {
+                Command::Uppercase => return_vector.push(tuple.0.to_uppercase()),
+                Command::Trim => return_vector.push(tuple.0.trim().to_string()),
+                Command::Append(n) => return_vector.push(append(tuple.0, n))
+            }
+        }
+        return return_vector;
+     }
+
+     fn append(input: String, count: usize) -> String {
+        if count == 1 {
+            input + "bar"
+        } else if count > 1 {
+            let input = input + "bar";
+            append(input, count-1)
+        }else{
+            input
+        }
+        // let return_string: String = String::new;
+        // for i in 1..count {
+        //     return_string = return_string + "bar"
+        // }
+        // return_string
+     }
 }
 
 fn main() {
     // You can optionally experiment here.
+    use my_module::transformer;
+        let input = vec![
+            ("hello".to_string(), Command::Uppercase),
+            (" all roads lead to rome! ".to_string(), Command::Trim),
+            ("foo".to_string(), Command::Append(1)),
+            ("bar".to_string(), Command::Append(5)),
+        ];
+        let output = transformer(input);
+        println!("{:?}",output)
 }
 
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
